0a70655a0602aec7c5132d63a696868f866714e5
DUSANI TEJA REDDY <tejareddydusani0@gmail.com>
2026-08-27 23:12:59 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,23 +1,5 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv1D, Flatten, Dense, Dropout
+from sklearn.ensemble import RandomForestClassifier
 
-def build_model(input_shape):
-    model = Sequential([
-        Conv1D(32, 2, activation='relu', input_shape=input_shape),
-        
-        Flatten(),
-        
-        Dense(64, activation='relu'),
-        Dropout(0.5),
-        
-        Dense(32, activation='relu'),
-        Dropout(0.3),
-        
-        Dense(1, activation='sigmoid')
-    ])
-
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
-
+def build_model(input_shape=None):
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
     return model
